thalinda-oss/Gitgit

commit bf93c2d36d2cebbd1144c7d17947f2d5e479dca5

gitweb: Convert project name to UTF-8
If the repository directory name is in non-ascii, $project needs to be
converted from perl internal to utf-8 because it will be used as
title, page path, and snapshot filename.

use to_utf8() to do the conversion.

Signed-off-by: Junio C Hamano <junkio@cox.net>

gitweb/gitweb.perl
@@ -1690,7 +1690,7 @@ sub git_header_html {
 
 	my $title = "$site_name";
 	if (defined $project) {
-		$title .= " - $project";
+		$title .= " - " . to_utf8($project);
 		if (defined $action) {
 			$title .= "/$action";
 			if (defined $file_name) {
@@ -1963,7 +1963,7 @@ sub git_print_page_path {
 
 	print "<div class=\"page_path\">";
 	print $cgi->a({-href => href(action=>"tree", hash_base=>$hb),
-	              -title => 'tree root'}, "[$project]");
+	              -title => 'tree root'}, to_utf8("[$project]");
 	print " / ";
 	if (defined $name) {
 		my @dirname = split '/', $name;
@@ -3610,7 +3610,7 @@ sub git_snapshot {
 		$hash = git_get_head_hash($project);
 	}
 
-	my $filename = basename($project) . "-$hash.tar.$suffix";
+	my $filename = to_utf8(basename($project)) . "-$hash.tar.$suffix";
 
 	print $cgi->header(
 		-type => "application/$ctype",

commit 3b4f97d7e945ac43fdfaefbdaed1c7f9029675ed

gitweb: fix mismatched parenthesis
An earlier commit bf93c2d3 broke gitweb.  Badly.

Signed-off-by: Junio C Hamano <junkio@cox.net>

gitweb/gitweb.perl
@@ -1963,7 +1963,7 @@ sub git_print_page_path {
 
 	print "<div class=\"page_path\">";
 	print $cgi->a({-href => href(action=>"tree", hash_base=>$hb),
-	              -title => 'tree root'}, to_utf8("[$project]");
+	              -title => 'tree root'}, to_utf8("[$project]"));
 	print " / ";
 	if (defined $name) {
 		my @dirname = split '/', $name;
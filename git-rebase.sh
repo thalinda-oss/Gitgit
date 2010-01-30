thalinda-oss/Gitgit

commit abf23117074472de8990b7b2a276b3b2cac57a84

rebase: replace antiquated sed invocation
Use the modern form of printing a commit subject instead of piping
the output of rev-list to sed.

Signed-off-by: Stephen Boyd <bebarino@gmail.com>
Signed-off-by: Junio C Hamano <gitster@pobox.com>

git-rebase.sh
@@ -85,10 +85,8 @@ continue_merge () {
 			printf "Already applied: %0${prec}d " $msgnum
 		fi
 	fi
-	if test -z "$GIT_QUIET"
-	then
-		git rev-list --pretty=oneline -1 "$cmt" | sed -e 's/^[^ ]* //'
-	fi
+	test -z "$GIT_QUIET" &&
+	git log --format=%s -1 "$cmt"
 
 	prev_head=`git rev-parse HEAD^0`
 	# save the resulting commit so we can read-tree on it later

commit 4e49d707badadd9a8a050727e2b1541550fa6c69

rebase: don't invoke the pager for each commit summary
This regression was introduced by commit abf2311 (rebase: replace
antiquated sed invocation, 2010-01-24), which changed the invocation of
"git rev-list | sed" to "git log".

It can be reproduced by something like this:
$ git rebase -s recursive origin/master

Signed-off-by: Markus Heidelberg <markus.heidelberg@web.de>
Signed-off-by: Junio C Hamano <gitster@pobox.com>

git-rebase.sh
@@ -86,7 +86,7 @@ continue_merge () {
 		fi
 	fi
 	test -z "$GIT_QUIET" &&
-	git log --format=%s -1 "$cmt"
+	GIT_PAGER='' git log --format=%s -1 "$cmt"
 
 	prev_head=`git rev-parse HEAD^0`
 	# save the resulting commit so we can read-tree on it later
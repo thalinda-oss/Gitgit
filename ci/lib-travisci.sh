thalinda-oss/Gitgit

commit 3da7326a6b9261b774a6e3f85be8f0dcab118dc2

travis-ci: skip a branch build if equal tag is present
If we push a branch and a tag pointing to the HEAD of this branch,
then Travis CI would run the build twice. This wastes resources and
slows the testing.

Add a function to detect this situation and skip the build the branch
if appropriate. Invoke this function on every build.

Helped-by: Junio C Hamano <gitster@pobox.com>
Signed-off-by: Lars Schneider <larsxschneider@gmail.com>
Signed-off-by: Junio C Hamano <gitster@pobox.com>

ci/lib-travisci.sh
@@ -1,5 +1,28 @@
 # Library of functions shared by all CI scripts
 
+skip_branch_tip_with_tag () {
+	# Sometimes, a branch is pushed at the same time the tag that points
+	# at the same commit as the tip of the branch is pushed, and building
+	# both at the same time is a waste.
+	#
+	# Travis gives a tagname e.g. v2.14.0 in $TRAVIS_BRANCH when
+	# the build is triggered by a push to a tag.  Let's see if
+	# $TRAVIS_BRANCH is exactly at a tag, and if so, if it is
+	# different from $TRAVIS_BRANCH.  That way, we can tell if
+	# we are building the tip of a branch that is tagged and
+	# we can skip the build because we won't be skipping a build
+	# of a tag.
+
+	if TAG=$(git describe --exact-match "$TRAVIS_BRANCH" 2>/dev/null) &&
+		$TAG != $TRAVIS_BRANCH
+	then
+		echo "Tip of $TRAVIS_BRANCH is exactly at $TAG"
+		exit 0
+	fi
+}
+
 # Set 'exit on error' for all CI scripts to let the caller know that
 # something went wrong
 set -e
+
+skip_branch_tip_with_tag

commit 47978c96f1ce273f327194f6d466d550bbe60038

travis-ci: fix "skip_branch_tip_with_tag()" string comparison
3da7326 ("travis-ci: skip a branch build if equal tag is present",
2017-09-17) introduced the "skip_branch_tip_with_tag" function with
a broken string comparison. Fix it!

Reported-by: SZEDER Gábor <szeder.dev@gmail.com>
Signed-off-by: Lars Schneider <larsxschneider@gmail.com>
Signed-off-by: Junio C Hamano <gitster@pobox.com>

ci/lib-travisci.sh
@@ -14,7 +14,7 @@ skip_branch_tip_with_tag () {
 	# of a tag.
 
 	if TAG=$(git describe --exact-match "$TRAVIS_BRANCH" 2>/dev/null) &&
-		$TAG != $TRAVIS_BRANCH
+		test "$TAG" != "$TRAVIS_BRANCH"
 	then
 		echo "Tip of $TRAVIS_BRANCH is exactly at $TAG"
 		exit 0